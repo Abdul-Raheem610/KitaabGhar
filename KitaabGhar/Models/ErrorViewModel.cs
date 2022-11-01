7584ebc9995f748498388ce324a912cbb89c9d76
Abdul Raheem <Abdul Raheem@DESKTOP-2NFPHQD>
2022-11-01 22:23:20 +0500

    Add project files.

---
+++ KitaabGhar/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace KitaabGhar.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
